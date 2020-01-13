203a51c74e3c02235ed4b264a2f6616ec56e56eb
zhangshenghu <zhangshenghu@sensetime.com>
2020-01-13 08:40:56 +0800

    add consul

--- mango-backup/src/main/java/com/choupijiang/mango/backup/MangoBackupApplication.java
+++ mango-backup/src/main/java/com/choupijiang/mango/backup/MangoBackupApplication.java
@@ -3,6 +3,8 @@ package com.choupijiang.mango.backup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication(scanBasePackages={"com.choupijiang.mango"})
 public class MangoBackupApplication {
 
